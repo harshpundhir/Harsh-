ae8495c6c78a2bee3cbaff3349ce5ae261bd9297
Harsh Pundhir <58081276+harshpundhir@users.noreply.github.com>
2024-05-25 13:13:20 +0200

    svelte_fastAPI

---
+++ Frontend/Svelte/svelte_fastapi.svelte
@@ -0,0 +1,27 @@
+<script>
+let potat=2
+let tomat=4
+
+//sending data
+async function sendData(){
+
+    const payload = {potat, tomat}; //payload the 
+    try {
+        // HACK: await fetch (url, {method: val, headers: val, body: val})
+        const response = await fetch('http://fastapiLink.com/this_endpoint', {
+            methods: 'POST',
+            headers: { 'Conetent-Type : 'application/json'},
+            body: JSON.stringify(payload)
+
+        });
+
+        if (!response.ok) { throw new Error('Not Okay')}
+
+        const jsonresponse = await response.json() // .josn() to get just json info, no metadata
+
+    } catch (error) {
+        console.error('error',error)
+    }
+}
+
+</script>
